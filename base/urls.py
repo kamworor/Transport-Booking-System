f8ecd1a7b0c0a170f4c235f545516bb28f05c8ae
kamworor <51693048+kamworor@users.noreply.github.com>
2023-06-05 23:22:01 +0300

    first commit

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('login/', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'), 
+    path('logout/', views.logoutUser, name='logout'), 
+    path('register/', views.registerUser, name='register'),
     # Add more URL patterns here
 ]
